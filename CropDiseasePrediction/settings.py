c9520a89bbd4532af72457520b4326da241419f3
Ruhaan Hawaldar <ruhaanhawaldar@gmail.com>
2025-04-27 09:14:32 +0530

    done1

--- CropDiseasePrediction/settings.py
+++ CropDiseasePrediction/settings.py
@@ -142,3 +142,7 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# LOGIN_URL = '/yoloapp/login/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
